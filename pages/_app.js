5d540f01b0abff4c2243428f758a478bc4f584d8
saintskeeper <saintskeeper@gmail.com>
2021-07-29 19:41:54 -0400

    adding data model changes to the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,21 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
+import { UserContext } from '../lib/context';
+import { useUserData } from '../lib/hooks';
 
 
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
+
   return (
     <>
+    <UserContext.Provider value={{ user:{}, username: 'jeff'}}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
+      </UserContext.Provider>
     </>
   );
 }
